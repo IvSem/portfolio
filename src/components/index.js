2ac6fc81c7f3344ba79f10fa96f203df1a9b74a2
IvSem <livansll96@gmail.com>
2023-05-16 20:09:30 +0300

    update main

--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,3 @@
 export { default as Sidebar } from './Sidebar/Sidebar.jsx';
 export { default as AnimatedLetters } from './AnimatedLetters/AnimatedLetters';
+export { default as Logo } from './Logo/Logo';
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,63 +1,134 @@
-import LogoTitle from 'assets/images/logo-s.png';
-import { AnimatedLetters } from 'components';
 import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+//import Loader from 'react-loaders';
+import LogoTitle from 'assets/images/logo-s.png';
 import './Home.scss';
+import { AnimatedLetters, Logo } from 'components';
 
 const Home = () => {
 	const [letterClass, setLetterClass] = useState('text-animate');
 
-	const nameArray = [...'umenko'];
-	const jobArray = [...'Front-End Dev.'];
+	const nameArray = ['l', 'o', 'b', 'o', 'd', 'a', 'n'];
+	const jobArray = [
+		'w',
+		'e',
+		'b',
+		' ',
+		'd',
+		'e',
+		'v',
+		'e',
+		'l',
+		'o',
+		'p',
+		'e',
+		'r',
+		'.',
+	];
 
-	//useEffect(() => {
-	//	let timeoutId;
-	//	timeoutId = setTimeout(() => {
-	//		setLetterClass('text-animate-hover');
-	//	}, 4000);
-
-	//	return () => {
-	//		clearTimeout(timeoutId);
-	//	};
-	//}, []);
 	useEffect(() => {
 		const timer = setTimeout(() => {
+			//setLetterClass('text-animate');
 			setLetterClass('text-animate-hover');
 		}, 4000);
 
 		return () => clearTimeout(timer);
 	}, []);
+
 	return (
-		<div className="container home-page">
-			<div className="text-zone">
-				<h1>
-					<span className={letterClass}>H</span>
-					<span className={`${letterClass} _12`}>i</span>
-					<span className={`${letterClass} _13`}>,</span>
-					<br />
-					<span className={`${letterClass} _14`}>I</span>
-					<span className={`${letterClass} _15`}>’</span>
-					<span className={`${letterClass} _16`}>m</span>
-					<img src={LogoTitle} alt="dev" />
-					<AnimatedLetters
-						letterClass={letterClass}
-						strArray={nameArray}
-						idx={16}
-					/>
-					<br />
-					<AnimatedLetters
-						letterClass={letterClass}
-						strArray={jobArray}
-						idx={22}
-					/>
-				</h1>
-				<h2>Front-End Developer / Freelancer</h2>
-				<Link to="/contact" className="flat-button">
-					CONTACT ME
-				</Link>
+		<>
+			<div className="container home-page">
+				<div className="text-zone">
+					<h1>
+						<span className={letterClass}>H</span>
+						<span className={`${letterClass} _12`}>i,</span>
+						<br />
+						<span className={`${letterClass} _13`}>I</span>
+						<span className={`${letterClass} _14`}>'m</span>
+						<img
+							src={LogoTitle}
+							alt="JavaScript Developer Name, Web Developer Name"
+						/>
+						<AnimatedLetters
+							letterClass={letterClass}
+							strArray={nameArray}
+							idx={15}
+						/>
+						<br />
+						<AnimatedLetters
+							letterClass={letterClass}
+							strArray={jobArray}
+							idx={22}
+						/>
+					</h1>
+					<h2>Front End Developer / JavaScript Expert / Youtuber</h2>
+					<Link to="/contact" className="flat-button">
+						CONTACT ME
+					</Link>
+				</div>
+				<Logo />
 			</div>
-		</div>
+
+			{/*<Loader type="pacman" />*/}
+		</>
 	);
 };
 
 export default Home;
+
+//import LogoTitle from 'assets/images/logo-s.png';
+//import { AnimatedLetters, Logo } from 'components';
+//import { useEffect, useState } from 'react';
+//import { Link } from 'react-router-dom';
+//import './Home.scss';
+
+//const Home = () => {
+//	const [letterClass, setLetterClass] = useState('text-animate');
+
+//	const nameArray = [...'umenko'];
+//	const jobArray = [...'Front-End Dev.'];
+
+//	useEffect(() => {
+//		const timer = setTimeout(() => {
+//			setLetterClass('text-animate');
+//			//setLetterClass('text-animate-hover');
+//		}, 4000);
+
+//		return () => clearTimeout(timer);
+//	}, []);
+//	return (
+//		<div className="container home-page">
+//			<div className="text-zone">
+//				<h1>
+//					<span className={letterClass}>H</span>
+//					<span className={`${letterClass} _12`}>i</span>
+//					<span className={`${letterClass} _13`}>,</span>
+//					<br />
+//					<span className={`${letterClass} _14`}>I</span>
+//					<span className={`${letterClass} _15`}>’</span>
+//					<span className={`${letterClass} _16`}>m</span>
+//					<img src={LogoTitle} alt="dev" />
+//					<AnimatedLetters
+//						letterClass={letterClass}
+//						strArray={nameArray}
+//						idx={16}
+//					/>
+//					<br />
+//					<AnimatedLetters
+//						letterClass={letterClass}
+//						strArray={jobArray}
+//						idx={22}
+//					/>
+//				</h1>
+//				<h2>Front-End Developer / Freelancer</h2>
+//				<Link to="/contact" className="flat-button">
+//					CONTACT ME
+//				</Link>
+//			</div>
+
+//			<Logo />
+//		</div>
+//	);
+//};
+
+//export default Home;
